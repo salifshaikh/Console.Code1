dd31198cdb46cad18a2f71dd4ec5845646a17655
Salif Shaikh <shaikhsalif50@gmail.com>
2025-01-07 02:50:42 +0530

    return

--- next.config.js
+++ next.config.js
@@ -1,39 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
+    domains: ["localhost"],
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost", // Replace this if you're using a production hostname
-        port: "3000", // Add port for localhost (if applicable)
-      },
-      {
         protocol: "https",
-        hostname: "cdn.sanity.io", // Example for external image sources
+        hostname: "cdn.sanity.io",
         port: "",
       },
     ],
   },
+};
+module.exports = {
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: '/:path*',
         headers: [
           {
-            key: "Content-Security-Policy",
+            key: 'Content-Security-Policy',
             value: "frame-src 'self' https://*.twilio.com",
           },
         ],
       },
     ];
   },
+};
+
+module.exports = {
+  reactStrictMode: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
     }
     return config;
   },
-};
+}
 
 module.exports = nextConfig;
